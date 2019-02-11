a0a957c9b1a2557ccf44f52ea9a3dcdaf2a026b4
Jacob Baker <baker210@mail.chapman.edu>
2019-02-11 01:59:33 +0000

    Broken, 2/10/19

--- DNA.cpp
+++ DNA.cpp
@@ -5,15 +5,20 @@ using namespace std;
 
 int main(){
 
-    ifstream file ("file.txt");
+    ifstream file ("test.txt"); //opens file for reading
     string line;
 
-    while(getline(file, line)){
+    while(getline(file, line)){ //iterates through each line of the file
         cout << "printed becuase of a line" <<endl;
 
     }
 
-    file.close();
+    file.close(); //close file when done
+
+    ofstream outfile ("jacobbaker.out"); //creates new file yourname.out
+
+    outfile << "test text" << endl; //prints "test text"
+    outfile << "test text 2" << endl;
 
     return 0;
 
