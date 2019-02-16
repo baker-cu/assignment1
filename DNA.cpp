b8c603185588a911fd7bcb007cf22245f44c16d8
Jacob Baker <baker210@mail.chapman.edu>
2019-02-16 07:35:39 +0000

    very broken

--- DNA.cpp
+++ DNA.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
+#include <math.h>
 
 using namespace std;
 
@@ -10,15 +11,15 @@ int main(int argc, char* argv[]){
     string init_line = "";
     string line = "";
     string new_line = "";
-    int leng = 0;
-    int new_leng = 0;
-    int num_nucs = 0; //number of nuceleotides
-    int num_lines = 0; //number of lines
-    int a_count = 0, t_count = 0, g_count = 0, c_count = 0;
-    int aa_count = 0, at_count = 0, ag_count = 0, ac_count = 0;
-    int tt_count = 0, ta_count = 0, tg_count = 0, tc_count = 0;
-    int gg_count = 0, ga_count = 0, gt_count = 0, gc_count = 0;
-    int cc_count = 0, ca_count = 0, ct_count = 0, cg_count = 0;
+    float leng = 0;
+    float new_leng = 0;
+    float num_nucs = 0; //number of nuceleotides
+    float num_lines = 0; //number of lines
+    float a_count = 0, t_count = 0, g_count = 0, c_count = 0;
+    float aa_count = 0, at_count = 0, ag_count = 0, ac_count = 0;
+    float tt_count = 0, ta_count = 0, tg_count = 0, tc_count = 0;
+    float gg_count = 0, ga_count = 0, gt_count = 0, gc_count = 0;
+    float cc_count = 0, ca_count = 0, ct_count = 0, cg_count = 0;
     int even_odd; // 1 = odd, 0 = even
     float sum = 0.0, mean = 0.0, var = 0.0, sd = 0.0;
 
@@ -46,14 +47,12 @@ int main(int argc, char* argv[]){
 
         for(int i = 0; i < new_leng; i++){ //for loop to iterate through string and read letters
 
-            even_odd == i % 2;
+            even_odd = i % 2;
             if(even_odd == 0){ //index is even
-                cout<<line[i]<<endl;
 
                 if(line[i] == 'a'){ //first
                     a_count++;
                     num_nucs++;
-                    cout<<"in a counter"<<endl;
 
                     //when when an even number the dimer is checked and added to the count
                     if(line[i + 1] == 'a'){
@@ -150,7 +149,7 @@ int main(int argc, char* argv[]){
             }
 
             else{
-                cout<<line[i]<<endl;
+
                 if(line[i] == 'a'){ //first
                     a_count++;
                     num_nucs++;
@@ -197,10 +196,9 @@ int main(int argc, char* argv[]){
                     }
                 }
                 new_leng = line.length();
-                cout<<new_leng<<"new leng"<<endl;
-                cout<<mean<<"mean"<<endl;
+
                 sd_num += ((new_leng-mean)*(new_leng-mean));
-                cout<<sd_num<<"sdnum"<<endl;
+
             }
 
     file1.close(); //close file when done
@@ -215,27 +213,26 @@ int main(int argc, char* argv[]){
     sd = sqrt(var);
 
     //probabilities
-    int a_prob = a_count/num_nucs;
-    cout<<a_prob<<endl;
-    int t_prob = t_count/num_nucs;
-    int g_prob = g_count/num_nucs;
-    int c_prob = c_count/num_nucs;
-    int aa_prob = aa_count/(num_nucs/2);
-    int at_prob = at_count/(num_nucs/2);
-    int ag_prob = ag_count/(num_nucs/2);
-    int ac_prob = ac_count/(num_nucs/2);
-    int tt_prob = tt_count/(num_nucs/2);
-    int ta_prob = ta_count/(num_nucs/2);
-    int tc_prob = tc_count/(num_nucs/2);
-    int tg_prob = tg_count/(num_nucs/2);
-    int cc_prob = cc_count/(num_nucs/2);
-    int ca_prob = ca_count/(num_nucs/2);
-    int ct_prob = ct_count/(num_nucs/2);
-    int cg_prob = cg_count/(num_nucs/2);
-    int gg_prob = gg_count/(num_nucs/2);
-    int ga_prob = ga_count/(num_nucs/2);
-    int gt_prob = gt_count/(num_nucs/2);
-    int gc_prob = gc_count/(num_nucs/2);
+    float a_prob = a_count/num_nucs;
+    float t_prob = t_count/num_nucs;
+    float g_prob = g_count/num_nucs;
+    float c_prob = c_count/num_nucs;
+    float aa_prob = aa_count/(num_nucs/2);
+    float at_prob = at_count/(num_nucs/2);
+    float ag_prob = ag_count/(num_nucs/2);
+    float ac_prob = ac_count/(num_nucs/2);
+    float tt_prob = tt_count/(num_nucs/2);
+    float ta_prob = ta_count/(num_nucs/2);
+    float tc_prob = tc_count/(num_nucs/2);
+    float tg_prob = tg_count/(num_nucs/2);
+    float cc_prob = cc_count/(num_nucs/2);
+    float ca_prob = ca_count/(num_nucs/2);
+    float ct_prob = ct_count/(num_nucs/2);
+    float cg_prob = cg_count/(num_nucs/2);
+    float gg_prob = gg_count/(num_nucs/2);
+    float ga_prob = ga_count/(num_nucs/2);
+    float gt_prob = gt_count/(num_nucs/2);
+    float gc_prob = gc_count/(num_nucs/2);
 
     ofstream outfile("jacobbaker.out"); //creates new file yourname.out
 
@@ -268,6 +265,53 @@ int main(int argc, char* argv[]){
     outfile << "Probability of GA: " << ga_prob << endl;
     outfile << "Probability of GT: " << gt_prob << endl;
     outfile << "Probability of GC: " << gc_prob << endl;
+    outfile << "\n" << endl;
+
+    //float a = rand()
+    float a;
+    float b;
+    float c;
+    float x;
+    float n;
+    int d;
+
+    //int na = a_prob*100;
+    //int nt = t_prob*100;
+    //int nc = c_prob*100;
+    //int ng = g_prob*100;
+
+    for(int x = 1; x < 1000; x++){
+
+        a = ((double) rand() / (RAND_MAX));
+        b = ((double) rand() / (RAND_MAX));
+        c = sqrt((-2)*log(a))*cos(2*M_PI*b);
+        x = sd*c+mean;
+        //d = x; //d is the length of the line
+        //change x to an int d bc can't have decimal line length
+
+
+
+        for(int y = x; y>0; y--){
+            int r = (rand() % 4);
+            if(r == 0){
+                outfile<<"A";
+            }
+            if(r == 1){
+                outfile<<"T";
+            }
+            if(r == 2){
+                outfile<<"C";
+            }
+            if(r == 3){
+                outfile<<"G";
+            }
+        }
+
+        outfile<<endl;
+
+    }
+    outfile.close();
+
 
     return 0;
 
